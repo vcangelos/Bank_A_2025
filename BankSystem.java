c6453a8529c9392f91dc48470c0145000ea01bf6
RohanP4REKH <rohanparekh208@gmail.com>
2025-03-17 12:59:01 -0400

    Update BankSystem.java

--- BankSystem.java
+++ BankSystem.java
@@ -17,7 +17,7 @@ class Card {
         this.cardNumber = cardNumber;
     }
 
-    // Credit card network #
+// Credit card network #
     String getCardType() {
         return "Visa";
     }
@@ -30,7 +30,7 @@ class ExtendedCard extends Card {
         super(cardNumber);
     }
 
-    // CVC generator
+// CVC generator
     String generateCVC() {
         return String.format("%03d", random.nextInt(1000));
     }
@@ -52,7 +52,7 @@ class BankSecurity {
         return String.valueOf(1000 + random.nextInt(9000));
     }
 
-    // Hashes PIN
+// Hashes PIN
     public static String hashPin(String pin) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -67,7 +67,7 @@ class BankSecurity {
         }
     }
 
-    // Stores hashed pin
+// Stores hashed pin
     public static void setAccountPin(String pin) {
         storedHashedPin = hashPin(pin);
     }
@@ -89,7 +89,7 @@ public class BankSystem {
         System.out.print("Enter the cardholder's last name: ");
         String lastName = scanner.nextLine();
 
-        // Generate card
+// Generate card
         ExtendedCard card = new ExtendedCard(generateVisaCardNumber());
         System.out.println("\nCardholder Name: " + firstName + " " + lastName);
         System.out.println("Card Type: " + card.getCardType());
@@ -97,17 +97,17 @@ public class BankSystem {
         System.out.println("CVC: " + card.generateCVC());
         System.out.println("Expiration Date: " + card.generateExpirationDate());
 
-        // Generate PIN
+// Generate PIN
         String cardPin = BankSecurity.generateCardPin();
         System.out.println("Generated Card PIN: " + cardPin);
 
-        // "Set PIN"
+// "Set PIN"
         System.out.print("Set your Account PIN (4-digit): ");
         String accountPin = scanner.nextLine();
         BankSecurity.setAccountPin(accountPin);
         System.out.println("Account PIN set successfully.");
 
-        // Verify PIN
+// Verify PIN
         System.out.print("Enter your Account PIN to verify: ");
         String enteredPin = scanner.nextLine();
         if (BankSecurity.validatePin(enteredPin)) {
@@ -116,10 +116,10 @@ public class BankSystem {
             System.out.println("Incorrect PIN. Access denied.");
         }
 
-        // Write to CSV file
+// Write to CSV
         writeCardInfoToCSV(firstName, lastName, card.getCardType(), card.cardNumber, card.generateCVC(), card.generateExpirationDate(), cardPin, accountPin);
 
-        // Read and print the CSV file
+// Read/print the CSV
         try (Scanner csvScanner = new Scanner(new File("card_info.csv"))) {
             System.out.println("\nContents of card_info.csv:");
             while (csvScanner.hasNextLine()) {
@@ -132,7 +132,7 @@ public class BankSystem {
         scanner.close();
     }
 
-    // Generates Visa card number
+// Generates Visa card number
     private static String generateVisaCardNumber() {
         Random random = new Random();
         StringBuilder cardNumber = new StringBuilder("4");
@@ -142,7 +142,7 @@ public class BankSystem {
         return cardNumber.toString();
     }
 
-    // Writes card information to a CSV file
+// Writes card information to CSV
     private static void writeCardInfoToCSV(String firstName, String lastName, String cardType, String cardNumber, String cvc, String expirationDate, String cardPin, String accountPin) {
         try (PrintWriter writer = new PrintWriter(new FileWriter("card_info.csv", true))) {
             writer.println(firstName + "," + lastName + "," + cardType + "," + cardNumber + "," + cvc + "," + expirationDate + "," + cardPin + "," + accountPin);
